6fa815a9776369d0e5f31d1af57d1cdff43fcd18
mastourimalek <malekmastouri9@gmail.com>
2023-02-14 11:59:40 +0100

    first commit

---
+++ src/components/Adrees.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import gomy from "./gomy.PNG"
+const Adrees = () => {
+  return (
+    <div>
+        
+       
+        <a
+          target="_blank"
+          href="https://www.google.com/maps/place/GOMYCODE+Menzah+5/@36.8436781,10.169516,15z/data=!4m5!3m4!1s0x0:0x71a7d4d04a5b1ef2!8m2!3d36.8436781!4d10.169516"
+          rel="noreferrer"
+        >
+          <img src={gomy} className="icon2" alt="img" />
+        </a>
+    </div>
+  )
+}
+
+export default Adrees
